d5979a7c7541e830073125a45fdba1b115fa8310
Monica Pena <113930652+monica-ap@users.noreply.github.com>
2022-10-14 17:38:51 +0000

    random practice 1-3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import randint
 from random import shuffle
 from random import choice
+from random import random
   # random
   # Python comes with a built in random library. There are a lot of functions included in this random library, so we will only 
   #show you two useful functions for now.
@@ -37,3 +38,20 @@ else: print("number is odd")
 color=["red","blue","pink","purple"]
 random_color= choice(color)
 print(f"random color is {random_color}")
+
+# Random Practice #1
+  # Implement the randint() function from the random library that allows you to obtain an integer from 1 to 10, and store that value in a variable called random_number.
+random_number=randint(0,11)
+print(random_number)  
+  
+  # Random Practice #2
+  # Implement the random() function from the random library to obtain a real number between 0 and 1, and store that value in a variable called random_number.
+random_number=random()
+print(random_number)
+  
+  # Random Practice #3
+  # Use the random library's choice() method to get a random item from the list of names below, and store the chosen name in a variable called raffle.
+  
+names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
+raffle=choice(names)
+print(raffle)
